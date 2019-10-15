be0940a83276b625085c2b29205d9935a8e871f9
hacky.gapson <gapson@gmail.com>
2019-10-15 13:36:52 -0700

    action creation with delete, get contact, and post function

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,22 +29,27 @@ function Header(){
 //   </div>
 
 <nav class="navbar navbar-light navbar-expand-md custom-header">
-    <div class="container-fluid">
-        <a class="navbar-brand">
-            <span>P</span></a>BM<button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse"><span class="sr-only">
+    <div class="container-fluid list">
+        <div class="navbar-brand django">
+        <div className="call">
+         <i className="fas fa-phone"></i>
+         </div>
+                <h3 className="iconD"><span className="yellow">P</span>BM</h3>
+           <i className="fas fa-ellipsis-h point"></i>
+            <span></span></div><button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse"><span class="sr-only">
 
             </span><span class="navbar-toggler-icon">
 
             </span></button>
         
-            <ul class="nav navbar-nav ml-auto">
+            <ul class="nav navbar-nav ml-auto info">
                 <li class="nav-item dropdown">
-                    <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">
-                    <i className="fas fa-portrait profile"> class="dropdown-image"</i></a>
-                    <div class="dropdown-menu dropdown-menu-right" role="menu">
-                        <a class="dropdown-item" role="presentation" href="#">Settings </a>
-                        <a class="dropdown-item" role="presentation" href="#">Payments </a>
-                        <a class="dropdown-item" role="presentation" href="#">Logout </a></div>
+                    
+                    <i className="fas fa-portrait profile" />
+                    <label className="person"><b className="name">Admin Admin</b></label>
+                    <i className="fas fa-angle-down"></i>
+                        
+                        
                 </li>
             </ul>
     </div>
