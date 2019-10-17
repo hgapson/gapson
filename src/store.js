8c3b1b77727e149bf6c0f1d8900201cd31743f59
hacky.gapson <gapson@gmail.com>
2019-10-17 16:27:14 -0700

    deployement handle

--- src/store.js
+++ src/store.js
@@ -8,8 +8,9 @@ const middleware =[thunk];
 
 const store= createStore( rootReducer, initialState,
 compose(applyMiddleware(...middleware),
+window.navigator.userAgent.includes('Chrome') ?
 window.__REDUX_DEVTOOLS_EXTENSION__ &&
- window.__REDUX_DEVTOOLS_EXTENSION__()
+ window.__REDUX_DEVTOOLS_EXTENSION__() :compose,
 ));
 
 export default store;
